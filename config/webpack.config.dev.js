3d0f2da41bfa76a4167c3ee8e3b613215fc31608
komei otake <komei1220@gmail.com>
2017-11-12 13:22:33 +0900

    update webpack dev setting

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,55 @@
+const helpers = require('./helpers'),<
+  webpackConfig = require('./webpack.config.base'),<
+  HtmlWebpackPlugin = require('html-webpack-plugin'),<
+  DefinePlugin = require('webpack/lib/DefinePlugin'),<
+  env = require('../environment/dev.env');
+
+webpackConfig.module.rules = [...webpackConfig.module.rules,
+  {
+    test: /\.css$/,
+    loader: [
+      'css-loader', 'postcss-loader'
+    ]
+  },
+  {
+    test: /\.pug$/,
+    loader: 'pug-loader'
+  },
+  {
+    test: /\.(woff2?|svg)(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'url-loader?limit=10000'
+  },
+  {
+    test: /\.(ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+    loader: 'file-loader'
+  },
+  {
+    test: /\.(jpg|png)$/,
+    loader: 'url-loader'
+  }
+];
+
+webpackConfig.plugins = [...webpackConfig.plugins,<
+  new HtmlWebpackPlugin({<
+    inject: true,<
+    template: helpers.root('/src/index.html'),<
+    favicon: helpers.root('/static/favicon.ico')<
+  }),<
+    new DefinePlugin({<
+   'process.env': env<
+  })<
+];<
+
+webpackConfig.devServer = {<
+  port: 8080,<
+  host: 'localhost',<
+  historyApiFallback: true,<
+  watchOptions: {<
+    aggregateTimeout: 300,<
+    poll: 1000<
+  },<
+  contentBase: './src',<
+    open: true<
+  };<
+  
+  module.exports = webpackConfig;<
